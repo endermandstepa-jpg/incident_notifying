5682b1268f570f0e15a1bf967326bfc568c04516
work laptop <endermandstepa@gmail.com>
2026-06-05 21:03:42 +0300

    hotfix: repository syntax and remove an unserpervised charachter

--- src/main/java/com/emergency/alert/repository/UserResponseRepository.java
+++ src/main/java/com/emergency/alert/repository/UserResponseRepository.java
@@ -5,5 +5,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserResponseRepository extends JpaRepository<UserResponse, Long> {
 }
-
-List<UserResponse> findByNotificationId(Long notificationId);
